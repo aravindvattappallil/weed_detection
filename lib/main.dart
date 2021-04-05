4a43756b152fb5e07e3aee3821e5be8bfedd9688
Aravind Vattappallil <aravindvr99@gmail.com>
2021-04-06 00:07:05 +0530

    first flutter app build

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: "First App",
+      home: Scaffold(
+        body: Container(
+          child: Center(
+            child: Text("first app"),
+          ),
+        ),
+      ),
+    ),
+  );
+}
